90e42d3056384af5fb84895bc04563572776e531
Legida <Jackprime555@gmail.com>
2021-02-28 23:53:02 +0300

    SDL test #1

--- RogueOfDungeons/RogueOfDungeons.cpp
+++ RogueOfDungeons/RogueOfDungeons.cpp
@@ -1,19 +1,14 @@
-
 #include <iostream>
 #include <SDL.h>
-
+using namespace std;
 int main(int argc, char* argv[])
 {
-
-	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-	{
-		std::cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
-	}
-	else
-	{
-		std::cout << "SDL initialization succeeded!";
-	}
-
-	std::cin.get();
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window *window = SDL_CreateWindow( "title",SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1272, 720, SDL_WINDOW_SHOWN);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(renderer, 117, 187, 253, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(1000);
 	return 0;
 }

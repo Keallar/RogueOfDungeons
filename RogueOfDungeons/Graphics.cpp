0f7ccc2c96d4fdca4fda066507765cf89f2fa7be
Legida <Jackprime555@gmail.com>
2021-03-01 17:23:47 +0300

    Добавил картинку

--- RogueOfDungeons/Graphics.cpp
+++ RogueOfDungeons/Graphics.cpp
@@ -1,16 +1,34 @@
-#include "Graphics.h"
 #include <SDL.h>
+#include <SDL_Image.h>
+#include "Graphics.h"
+#include <iostream>
+
+const int WIDTH = 1280;
+const int HEIGHT = 720;
+
+
 
 Graphics::Graphics() {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	_window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1272, 720, SDL_WINDOW_SHOWN);
+	_window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 	_renderer = SDL_CreateRenderer(_window, -1, 0);
 }
 
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h)
+{
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	dst.w = w;
+	dst.h = h;
+	SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+
 void Graphics::SimpleWindow() {
-	SDL_SetRenderDrawColor(_renderer, 117, 187, 253, 255);
-	SDL_RenderClear(_renderer);
+	_backgroundMenu = IMG_LoadTexture(_renderer, "images/BackgroundMenu.png");
+	renderTexture(_backgroundMenu,_renderer, 0, 0, 1280, 720);
 	SDL_RenderPresent(_renderer);
+	SDL_RenderClear(_renderer);
 	SDL_Delay(3000);
 }
 void Graphics::Destroy() {
--- RogueOfDungeons/Graphics.h
+++ RogueOfDungeons/Graphics.h
@@ -1,9 +1,11 @@
 #pragma once
-struct SDL_Window;
 struct SDL_Renderer;
+struct SDL_Texture;
+struct SDL_Window;
 
 class Graphics {
 private:
+	SDL_Texture* _backgroundMenu;
 	SDL_Window* _window;
 	SDL_Renderer* _renderer;
 public:

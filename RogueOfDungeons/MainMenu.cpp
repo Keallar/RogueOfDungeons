07ba129408ce4df64af3bc7babaa761ee21b07b0
Eugene <zlysanskiy@gmail.com>
2021-03-04 10:35:26 +0300

    В MainMenu.cpp добавил недостающие методы

--- RogueOfDungeons/MainMenu.cpp
+++ RogueOfDungeons/MainMenu.cpp
@@ -1,9 +1,14 @@
 #include "MainMenu.h"
 #include "Game.h"
 #include "textureManager.h"
+
 MainMenu::MainMenu() {
 	MainMenuTexture = textureManager::LoadTexture("images/BackgroundMenu.png", renderer);
 }
+MainMenu::~MainMenu()
+{}
+void MainMenu::MainMenuTextureSeater()
+{}
 void MainMenu::Render() {
 	SDL_RenderCopy(renderer, MainMenuTexture, NULL, NULL);
 	SDL_RenderPresent(renderer);

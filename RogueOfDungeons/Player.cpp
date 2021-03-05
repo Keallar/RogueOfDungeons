0347ec3ed9af414e094efe6e914a8829387e21d6
zezzeebka <72992258+zezzeebka@users.noreply.github.com>
2021-03-05 17:21:35 +0300

    .

--- RogueOfDungeons/Player.cpp
+++ RogueOfDungeons/Player.cpp
@@ -1,27 +1,27 @@
-//#include "Player.h"
-//#include "GameObject.h"
-//
-//Player::Player(const char* texturesheet, SDL_Renderer* ren, int x, int y)
-//{
-//
-//}
-//
-//Player::~Player()
-//{
-//
-//}
-//
-//void Player::Render()
-//{
-//
-//}
-//
-//void Player::Update()
-//{
-//
-//}
-//
-//void Player::clean()
-//{
-//
-//}
+#include "Player.h"
+#include "GameObject.h"
+
+Player::Player(const char* texturesheet, SDL_Renderer* ren, int x, int y)
+{
+
+}
+
+Player::~Player()
+{
+
+}
+
+void Player::Render()
+{
+
+}
+
+void Player::Update()
+{
+
+}
+
+void Player::clean()
+{
+
+}

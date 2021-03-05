5e642be246b1c7ff6f2a46f0776a912a60845f85
Legida <Jackprime555@gmail.com>
2021-03-05 17:20:58 +0300

    нормально закомментил player

--- RogueOfDungeons/Player.h
+++ RogueOfDungeons/Player.h
@@ -1,27 +1,27 @@
-//#pragma once
-//#include "GameObject.h"
-//#include "Game.h"
-//
-//class Player :
-//    public GameObject
-//{
-//private:
-//    GameObject Player1;
-//    SDL_Texture* TileTexture;
-//    SDL_Renderer* ren;
-//
-//public:
-//    Player(const char* texturesheet, SDL_Renderer* ren, int x, int y);
-//    ~Player();
-//    void Render() {
-//        GameObject::Render();
-//    }
-//    void Update() {
-//        GameObject::Update();
-//    }
-//    void clean() {
-//        GameObject::clean();
-//    }
-//    bool flag = 0;
-//};
+/*#pragma once
+#include "GameObject.h"
+#include "Game.h"
+
+class Player :
+    public GameObject
+{
+private:
+    GameObject Player1;
+    SDL_Texture* TileTexture;
+    SDL_Renderer* ren;
+
+public:
+    Player(const char* texturesheet, SDL_Renderer* ren, int x, int y);
+    ~Player();
+    void Render() {
+        GameObject::Render();
+    }
+    void Update() {
+        GameObject::Update();
+    }
+    void clean() {
+        GameObject::clean();
+    }
+    bool flag = 0;
+};*/
 

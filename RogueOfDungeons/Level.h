cb8277642eb18de96857d695a961db40d69d762e
Eugene <zlysanskiy@gmail.com>
2021-03-08 11:24:47 +0300

    Перемести enum Тайлов в GameObject

--- RogueOfDungeons/Level.h
+++ RogueOfDungeons/Level.h
@@ -5,31 +5,6 @@
 #include "UI.h"
 #include "GameObject.h"
 
-//цвета тайлов
-enum Tiles
-{
-	white,
-	green,
-	darkYellowDirt,
-	black,
-	yellowSand,
-	brownDesks,
-	lightBrownWood,
-	lilac,
-	redBlood,
-	vinous,
-	blueWater,
-	orangeBrick,
-	greenDirt,
-	Dirt,
-	lightBrick,
-	portal1,
-	portal2
-};
-//turquoise = бирюзовый
-//vinous = винный, бордовый
-//lilac = сиреневый
-
 class Level 
 {
 private:

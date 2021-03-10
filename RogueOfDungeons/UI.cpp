d65808759d1b5f5d8b3ba48389d7994aa9dc398a
Eugene <zlysanskiy@gmail.com>
2021-03-10 21:30:50 +0300

    Добавил картинку HP

--- RogueOfDungeons/UI.cpp
+++ RogueOfDungeons/UI.cpp
@@ -17,7 +17,7 @@ UILevel::UILevel(SDL_Renderer* renderer)
 {
 	//SDL_Color color = { 255, 255, 255, 255 };
 	ren = renderer;
-	versionBLock = FontManager::renderText("ROGUE OF DUNGEONS V-0.0.1", "fonts/manaspc.ttf", 255, 255, 255, 255, 32, ren);
+	versionBLock = FontManager::renderText("ROGUE OF DUNGEONS V-0.0.2", "fonts/manaspc.ttf", 255, 255, 255, 255, 32, ren);
 	infoBlock = textureManager::LoadTexture("images/InfoBlock.png", ren);
 	inventoryBlock = textureManager::LoadTexture("images/InfoBlock.png", ren);
 	info = FontManager::renderText("Info", "fonts/manaspc.ttf", 255, 255, 255, 255, 32, ren);
@@ -26,7 +26,7 @@ UILevel::UILevel(SDL_Renderer* renderer)
 
 void UILevel::Render()
 {
-	RenderManager::CopyToRender(versionBLock, ren, EntityPosition::Coords[4], EntityPosition::Coords[5], 170, 10 );
+	RenderManager::CopyToRender(versionBLock, ren, EntityPosition::Coords[4], EntityPosition::Coords[5], 170, 9 );
 	RenderManager::CopyToRender(infoBlock, ren, EntityPosition::Coords[8], EntityPosition::Coords[9], 256, 480);
 	RenderManager::CopyToRender(inventoryBlock, ren, EntityPosition::Coords[10], EntityPosition::Coords[11], 256, 225);
 	RenderManager::CopyToRender(info, ren, EntityPosition::Coords[6], EntityPosition::Coords[7], 64, 32);

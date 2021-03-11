6e4ae082c5c76028b8170e226a927845e0e253b8
Eugene <zlysanskiy@gmail.com>
2021-03-11 14:42:32 +0300

    Добавил Mana Bar

--- RogueOfDungeons/UI.cpp
+++ RogueOfDungeons/UI.cpp
@@ -18,17 +18,36 @@ UILevel::UILevel(SDL_Renderer* renderer)
 	pathInFont = "fonts/manaspc.ttf";
 	SDL_Color color = { 255, 255, 255, 255 };
 	ren = renderer;
+
+	//Version
 	versionBLock = FontManager::renderText("ROGUE OF DUNGEONS V-0.0.2", pathInFont, color, 32, ren);
+	
 	infoBlock = textureManager::LoadTexture("images/InfoBlock.png", ren);
 	inventoryBlock = textureManager::LoadTexture("images/InfoBlock.png", ren);
 	info = FontManager::renderText("Info", "fonts/manaspc.ttf", color, 32, ren);
 	inventory = FontManager::renderText("Inventory", pathInFont, color, 32, ren);
+	
+	//HP
 	hpBar = textureManager::LoadTexture("images/hp.png", ren);
 	hpText = FontManager::renderText("HP", pathInFont, color, 64, ren);
 	hpInfo = FontManager::renderText("10/10", pathInFont, color, 32, ren);
+	
+	//Mana
+	manaBar = textureManager::LoadTexture("images/ManaBar.png", ren);
+	manaText = FontManager::renderText("MN", pathInFont, color, 64, ren);
+	manaInfo = FontManager::renderText("50/50", pathInFont, color, 32, ren);
+
+	//XP
 	xpBar = textureManager::LoadTexture("images/XP.png", ren);
 	xpText = FontManager::renderText("XP", pathInFont, color, 64, ren);
-	xpInfo = FontManager::renderText("100/100", pathInFont, color, 64, ren);
+	xpInfo = FontManager::renderText("99/100", pathInFont, color, 32, ren);
+
+	//Specifications
+	STR = FontManager::renderText("STR", pathInFont, color, 64, ren);
+	DEX = FontManager::renderText("DEX", pathInFont, color, 64, ren);
+	INT = FontManager::renderText("INT", pathInFont, color, 64, ren);
+	PHS = FontManager::renderText("INT", pathInFont, color, 64, ren);
+	LCK = FontManager::renderText("LCK", pathInFont, color, 64, ren);
 }
 
 void UILevel::Render()
@@ -38,11 +57,23 @@ void UILevel::Render()
 	RenderManager::CopyToRender(inventoryBlock, ren, 1024, 480, 256, 225);
 	RenderManager::CopyToRender(info, ren, 1116, 15, 64, 32);
 	RenderManager::CopyToRender(inventory, ren, 1085, 490, 128, 32);
-	RenderManager::CopyToRender(hpBar, ren, 1080, 40, 140, 32, 0, 0, 128, 16);
+	
+	//HP
+	RenderManager::CopyToRender(hpBar, ren, 1080, 40, 160, 32, 0, 0, 128, 16);
 	RenderManager::CopyToRender(hpText, ren, 1050, 47, 25, 22);
 	RenderManager::CopyToRender(hpInfo, ren, 1116, 72, 64, 20);
-	RenderManager::CopyToRender(xpBar, ren, 1080, 95, 180, 32, 21, 10, 128, 16);
-	RenderManager::CopyToRender(xpText, ren, 1050, 97, 25, 22);
-	RenderManager::CopyToRender(xpInfo, ren, 1115, 122, 72, 20);
+
+	//Mana
+	RenderManager::CopyToRender(manaBar, ren, 1080, 95, 230, 32, 21, 10, 128, 16);
+	RenderManager::CopyToRender(manaText, ren, 1050, 99, 25, 22);
+	RenderManager::CopyToRender(manaInfo, ren, 1116, 122, 64, 20);
+	
+	//XP
+	RenderManager::CopyToRender(xpBar, ren, 1080, 150, 200, 32, 21, 10, 128, 16);
+	RenderManager::CopyToRender(xpText, ren, 1050, 152, 25, 22);
+	RenderManager::CopyToRender(xpInfo, ren, 1116, 175, 64, 20);
+
+	//Specifications
+	//RenderManager::CopyToRender(STR, ren, 1050, 160, 64, 20);
 }
 
--- RogueOfDungeons/UI.h
+++ RogueOfDungeons/UI.h
@@ -26,12 +26,24 @@ private:
 	SDL_Texture* inventoryBlock;
 	SDL_Texture* info;
 	SDL_Texture* inventory;
+	//HP
 	SDL_Texture* hpBar;
 	SDL_Texture* hpText;
 	SDL_Texture* hpInfo;
+	//XP
 	SDL_Texture* xpBar;
 	SDL_Texture* xpText;
 	SDL_Texture* xpInfo;
+	//MANA
+	SDL_Texture* manaBar;
+	SDL_Texture* manaText;
+	SDL_Texture* manaInfo;
+	//Specifications
+	SDL_Texture* STR;
+	SDL_Texture* DEX;
+	SDL_Texture* INT;
+	SDL_Texture* PHS; //Physical Strength
+	SDL_Texture* LCK;
 public:
 	UILevel( SDL_Renderer* renderer);
 	void Render();

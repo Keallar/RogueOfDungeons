bd994042aec0590db25434897ff1fa2001c11d08
zezzeebka <72992258+zezzeebka@users.noreply.github.com>
2021-03-20 20:19:01 +0300

    Фикс спавна

--- RogueOfDungeons/Enemy.cpp
+++ RogueOfDungeons/Enemy.cpp
@@ -44,9 +44,12 @@ void Enemy::GetLoc(int arr[22][32])
 void Enemy::GetEnemyFirstCoords() {
 	EntityPosition::Coords[2] = (rand() % 30) * 32;
 	EntityPosition::Coords[3] = (rand() % 20) * 32;
-	while ((enemyLoc[EntityPosition::Coords[3] / 32][EntityPosition::Coords[2] / 32] == -2))
+	while ((enemyLoc[EntityPosition::Coords[3] / 32][EntityPosition::Coords[2] / 32] == -2) ||
+		((enemyLoc[EntityPosition::Coords[3] / 32][(EntityPosition::Coords[2] / 32) - 1] != -1) &&
+			(enemyLoc[EntityPosition::Coords[3] / 32][(EntityPosition::Coords[2] / 32) + 1] != -1) &&
+			(enemyLoc[(EntityPosition::Coords[3] / 32) - 1][EntityPosition::Coords[2] / 32] != -1) &&
+			(enemyLoc[(EntityPosition::Coords[3] / 32) + 1][EntityPosition::Coords[2] / 32] != -1)))
 	{
-		std::cout << "A";
 		EntityPosition::Coords[2] = (rand() % 30) * 32;
 		EntityPosition::Coords[3] = (rand() % 20) * 32;
 	}

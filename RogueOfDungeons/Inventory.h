8e924bd9197406726d8733a9d7dead0e82d27159
Legida <Jackprime555@gmail.com>
2021-03-20 21:56:13 +0300

    добавил классы для инвентаря

--- RogueOfDungeons/Inventory.h
+++ RogueOfDungeons/Inventory.h
@@ -2,11 +2,35 @@
 
 #include <string>
 #include <vector>
+#include <SDL.h>
+#include <map>
+
+enum type {
+	weapon,
+	armor,
+	other
+};
 
 class Inventory {
-	
+	std::map <int, InventoryItem> inventory;
+	std::map <int, InventoryItem> ExistingItems;
+
 };
 
 class InventoryItem {
-	
+public:
+	std::string name;
+	SDL_Texture* ItemTexture;
+	type Type;
+	std::string Description;
+};
+
+class Weapon : public InventoryItem {
+public:
+	int STR;
+	int AGI;
+	int INT;
+	int DMG;
+	bool rangeDamage;
+	int range;
 };

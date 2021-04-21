1a8897b575fb6c459e565f430953bafef70ef90d
Keallar <zlysanskiy@gmail.com>
2021-04-21 11:39:18 +0300

    Не работает вместе с Clion

---
+++ cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/Animation.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/Animation.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/Buttons.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/Buttons.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/CallBackAndHoverFuncs.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/CallBackAndHoverFuncs.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/Enemy.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/Enemy.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/EntityPosition.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/EntityPosition.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/Game.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/Game.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/GameObject.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/GameObject.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/GenerationMethods.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/GenerationMethods.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/Inventory.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/Inventory.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/Level.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/Level.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/MainMenu.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/MainMenu.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/Managers.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/Managers.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/Player.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/Player.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/RogueOfDungeons.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/RogueOfDungeons.cpp.obj"
+  "C:/Users/evgen/source/Projects/RogueOfDungeons/RogueOfDungeons/UI.cpp" "C:/Users/evgen/source/Projects/RogueOfDungeons/cmake-build-debug/CMakeFiles/RogueOfDungeons.dir/RogueOfDungeons/UI.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../RogueOfDungeons"
+  "../SDL2_image-2.0.5/include"
+  "../SDL2_ttf-2.0.15/include"
+  "../SDL2-2.0.14/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

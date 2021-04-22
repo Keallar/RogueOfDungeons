b3ce3f6388f54211b8ed932fd898de682f4cdac2
Balalarga <lightman03@mail.ru>
2021-04-22 16:31:35 +0300

    Update project structure

---
+++ src/Level.cpp
@@ -0,0 +1,468 @@
+﻿#pragma once
+#include "Game.h"
+#include "Level.h"
+#include "Managers.h"
+#include <ctime>
+#include "UI.h"
+#include "EntityPosition.h"
+#include <vector>
+#include <iostream>
+#include "EntityPosition.h"
+#include "Buttons.h"
+#include "Enemy.h"
+#include "Player.h"
+
+Level::Level(SDL_Renderer* renderer) : ren (renderer)
+{
+	floorLvl = 0;
+	TileTexture = textureManager::LoadTexture("data/images/Tiles.png", ren);
+	TileTextureCastle = textureManager::LoadTexture("data/images/CaslteTiles.png", ren);
+	PlayBackground = textureManager::LoadTexture("data/images/Playback.png", ren); 
+	player = new Player(ren);
+	enemyTurtle = new Enemy("data/images/Turtle.png", 4, ren, 8, 8, 3, 4);
+	uiEnemyHpInfo = new UiHpEnemyInfo(ren);
+	uiInfo = new UIInfo(ren);
+	uiItem = new UIItem(ren);
+	uiEnemy = new UIEnemyInfo(ren);
+	uiSpec = new UISpecifications(ren);
+	uiInv = new UIInventory(ren);
+	hp = new HpInfo(ren);
+	mana = new ManaInfo(ren);
+	exp = new ExpInfo(ren);
+	uiEquiped = new UIEquipedItem(ren);
+
+	//keyboard = new Keyboard();
+
+	for (int i = 0; i < 22; i++) 
+	{
+		for (int j = 0; j < 32; j++)
+		{
+			textureLocation[i][j] = 1;
+			Location[i][j] = 1;
+			Dark[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < 22; i++) 
+	{
+		for (int j = 0; j < 32; j++) 
+		{
+			if (i == 0 || i == 21 || j == 0 || j == 31)
+			{
+				Dark[i][j] = 1;
+			}
+		}
+	}
+}
+
+Level::~Level()
+{
+	//WTF нельзя делитнуть инвентарь (вылезает ошибка линковщика)
+	delete player;
+	delete enemyTurtle;
+	delete uiInfo;
+	delete uiItem;
+	delete uiEnemy;
+	delete uiSpec;
+	delete hp;
+	delete mana;
+	delete exp;
+	delete uiEquiped;
+	//delete keyboard;
+	delete animation;
+}
+
+void Level::deletePlayer()
+{
+	if (Player::GetHP(0) <= 0)
+	{
+		std::cout << "Delete player" << std::endl;
+		delete player;
+		player = nullptr;
+	}
+}
+
+void Level::deleteEnemy()
+{
+	if (Enemy::HP <= 0)
+	{
+		std::cout << "Delete enemy" << std::endl;
+		delete enemyTurtle;
+		enemyTurtle = nullptr;
+		FlagManager::flagUiEnemy = 0;
+		player->ChangeExpValue(5);
+	}
+}
+
+void Level::Update()
+{
+	int n = Player::VIS;
+	for (int i = (EntityPosition::Coords[1] / 32) - n; i <= (EntityPosition::Coords[1] / 32) + n; i++) {
+		for (int j = (EntityPosition::Coords[0] / 32) - n; j <= (EntityPosition::Coords[0] / 32) + n; j++) {
+			ChangeDark(i, j);
+		}
+	}
+	if (enemyTurtle == nullptr) {
+		FlagManager::flagEnemy = 0;
+		FlagManager::flagPlayer = 1;
+	}
+	if (player != nullptr && 
+		FlagManager::flagPlayer == 1 && FlagManager::flagEnemy == 0)
+	{
+		player->Update();
+	}
+	if (enemyTurtle != nullptr &&
+		FlagManager::flagPlayer == 0 && FlagManager::flagEnemy == 1)
+	{
+		enemyTurtle->Update();
+		enemyTurtle->GetLoc(Location);
+		SDL_Delay(100); //UNDONE перенести delay в Animation
+	}
+	if (player != nullptr)
+		player->GetLevel(Location);
+
+	if (enemyTurtle != nullptr)
+		enemyTurtle->GetLoc(Location);
+
+	//удаление player (enemy) при hp <= 0
+	if (Player::GetHP(0) <= 0 && player != nullptr)
+	{
+		Level::deletePlayer();
+	}
+	if (Enemy::HP <= 0 && enemyTurtle != nullptr)
+	{
+		Level::deleteEnemy();
+		EntityPosition::Coords[2] = 0;
+		EntityPosition::Coords[3] = 0;
+	}
+}
+
+void Level::Start()
+{
+	Generate();
+	player->GetLevel(Location);
+	player->GetPlayerFirstCoords();
+	enemyTurtle->GetLoc(Location);
+	enemyTurtle->GetEnemyFirstCoords();
+}
+
+void Level::ChangeDark(int i, int j) {
+	if (i >= 0 && i < 22 && j >= 0 && j < 32) {
+		Dark[i][j] = 1;
+	}
+}
+
+void Level::CaveRender(int y, int x) {
+	int i = y;
+	int j = x;
+	if (textureLocation[i][j] == 0)
+	{
+		RenderManager::SetTile(j * 32, i * 32, 2, ren, TileTexture);
+	}
+	else
+	{
+		RenderManager::SetTile(j * 32, i * 32, 6, ren, TileTexture);
+	}
+	if (textureLocation[i][j] == 2) {
+		RenderManager::SetTile(j * 32, i * 32, 7, ren, TileTexture);
+	}
+	if (textureLocation[i][j] == 3) {
+		RenderManager::SetTile(j * 32, i * 32, 3, ren, TileTexture);
+	}
+	if (textureLocation[i][j] == 4) {
+		RenderManager::SetTile(j * 32, i * 32, 1, ren, TileTexture);
+	}
+	if (textureLocation[i][j] == 5) {
+		RenderManager::SetTile(j * 32, i * 32, 9, ren, TileTexture);
+	}
+	if (textureLocation[i][j] == 6) {
+		RenderManager::SetTile(j * 32, i * 32, 4, ren, TileTexture);
+	}
+	if (textureLocation[i][j] == 14) {
+		RenderManager::SetTile(j * 32, i * 32, 13, ren, TileTexture);
+	}
+	if (Dark[i][j] == 0) {
+		RenderManager::SetTile(j * 32, i * 32, 12, ren, TileTexture);
+	}
+}
+
+void Level::CastleRender(int y, int x) {
+	int i = y;
+	int j = x;
+	if (textureLocation[i][j] == 0)
+	{
+		RenderManager::SetTile(j * 32, i * 32, 6, ren, TileTextureCastle);
+	}
+	else
+	{
+		RenderManager::SetTile(j * 32, i * 32, 8, ren, TileTextureCastle);
+	}
+	if (textureLocation[i][j] == 2) {
+		RenderManager::SetTile(j * 32, i * 32, 9, ren, TileTextureCastle);
+	}
+	if (textureLocation[i][j] == 3) {
+		RenderManager::SetTile(j * 32, i * 32, 2, ren, TileTextureCastle);
+	}
+	if (textureLocation[i][j] == 4) {
+		RenderManager::SetTile(j * 32, i * 32, 3, ren, TileTextureCastle);
+	}
+	if (textureLocation[i][j] == 5) {
+		RenderManager::SetTile(j * 32, i * 32, 7, ren, TileTextureCastle);
+	}
+	if (textureLocation[i][j] == 6) {
+		RenderManager::SetTile(j * 32, i * 32, 1, ren, TileTextureCastle);
+	}
+	if (textureLocation[i][j] == 7) {
+		RenderManager::SetTile(j * 32, i * 32, 10, ren, TileTextureCastle);
+	}
+	if (textureLocation[i][j] == 8) {
+		RenderManager::SetTile(j * 32, i * 32, 11, ren, TileTextureCastle);
+	}
+	if (textureLocation[i][j] == 14) {
+		RenderManager::SetTile(j * 32, i * 32, 14, ren, TileTextureCastle);
+	}
+	if (Dark[i][j] == 0) {
+		RenderManager::SetTile(j * 32, i * 32, 12, ren, TileTexture);
+	}
+}
+
+void Level::Render()
+{
+	RenderManager::CopyToRender(PlayBackground, ren);
+	
+	//в зависимости от метода генерации выбираются нужные паки текстур
+	if ((generateChoose == 0) || (generateChoose == 2) || (generateChoose == 4)) {
+		for (int i = 0; i < 22; i++)
+		{
+			for (int j = 0; j < 32; j++)
+			{
+				CaveRender(i, j);
+			}
+		}
+	}
+
+	if ((generateChoose == 1)||(generateChoose == 5)) {
+		for (int i = 0; i < 22; i++)
+		{
+			for (int j = 0; j < 32; j++)
+			{
+				CastleRender(i, j);
+			}
+		}
+	}
+	if (player != nullptr)
+	{
+		player->Render();
+	}		
+	if (enemyTurtle != nullptr)
+	{
+		if (Dark[EntityPosition::Coords[3]/32][EntityPosition::Coords[2]/32] == 0) 
+		{
+			RenderManager::SetTile(EntityPosition::Coords[2], EntityPosition::Coords[3], 12, ren, TileTexture);
+		}
+		else
+		{
+			enemyTurtle->Render();
+		}
+	}
+
+	//ALL UI
+	{
+		uiItem->Render();
+		uiInfo->AlwaysRender();
+		uiEquiped->Render();
+
+		if (FlagManager::flagInv == 1)
+		{
+			uiInv->Render();
+		}
+
+		if (FlagManager::flagUiSpec == 1)
+		{
+			uiSpec->Render();
+
+			if (FlagManager::flagSTR == 1)
+			{
+				uiSpec->Update(Player::GetSpecValue(1), 1);
+			}
+			if (FlagManager::flagDEX == 1)
+			{
+				uiSpec->Update(Player::GetSpecValue(2), 2);
+			}
+			if (FlagManager::flagINT == 1)
+			{
+				uiSpec->Update(Player::GetSpecValue(3), 3);
+				mana->UpdateMax();
+			}
+			if (FlagManager::flagWSD == 1)
+			{
+				uiSpec->Update(Player::GetSpecValue(4), 4);
+			}
+			if (FlagManager::flagPHS == 1)
+			{
+				uiSpec->Update(Player::GetSpecValue(5), 5);
+				hp->UpdateMax();
+			}
+			if (FlagManager::flagLCK == 1)
+			{
+				uiSpec->Update(Player::GetSpecValue(6), 6);
+			}
+		}
+
+		if (FlagManager::flagUI == 1)
+		{
+			uiInfo->Render();
+			hp->Render();
+			mana->Render();
+			exp->Render();
+
+			if (FlagManager::flagCheckHpEnemy == 1)
+			{
+				uiEnemyHpInfo->Update();
+			}
+
+			if (FlagManager::flagUiEnemy == 1)
+			{
+				uiEnemy->Render();
+				uiEnemyHpInfo->Render();
+			}
+
+			//Update значений hp, mana и  exp
+			if (FlagManager::flagCheckHP == 1)
+			{
+				hp->Update();
+			}
+
+			if (FlagManager::flagCheckMana == 1)
+			{
+				mana->Update();
+			}
+
+			if (FlagManager::flagCheckExp == 1)
+			{
+				exp->Update();
+			}
+		}
+
+		if (FlagManager::flagChest != 0) {
+			if (!player->InventoryBlock()) {
+				switch (FlagManager::flagChest) {
+				case 1:
+					textureLocation[(EntityPosition::Coords[1]) / 32 - 1][(EntityPosition::Coords[0]) / 32] = 0;
+					Location[(EntityPosition::Coords[1]) / 32 - 1][(EntityPosition::Coords[0]) / 32] = 0;
+					player->GetItemOnLvl(itemsOnLvl[itemsHave]);
+					itemsHave--;
+					FlagManager::flagChest = 0;
+					break;
+				case 2:
+					textureLocation[(EntityPosition::Coords[1]) / 32][(EntityPosition::Coords[0]) / 32 - 1] = 0;
+					Location[(EntityPosition::Coords[1]) / 32][(EntityPosition::Coords[0]) / 32 - 1] = 0;
+					player->GetItemOnLvl(itemsOnLvl[itemsHave]);
+					itemsHave--;
+					FlagManager::flagChest = 0;
+					break;
+				case 3:
+					textureLocation[(EntityPosition::Coords[1]) / 32 + 1][(EntityPosition::Coords[0]) / 32] = 0;
+					Location[(EntityPosition::Coords[1]) / 32 + 1][(EntityPosition::Coords[0]) / 32] = 0;
+					player->GetItemOnLvl(itemsOnLvl[itemsHave]);
+					itemsHave--;
+					FlagManager::flagChest = 0;
+					break;
+				case 4:
+					textureLocation[(EntityPosition::Coords[1]) / 32][(EntityPosition::Coords[0]) / 32 + 1] = 0;
+					Location[(EntityPosition::Coords[1]) / 32][(EntityPosition::Coords[0]) / 32 + 1] = 0;
+					player->GetItemOnLvl(itemsOnLvl[itemsHave]);
+					itemsHave--;
+					FlagManager::flagChest = 0;
+					break;
+				}
+			}
+			else {
+				FlagManager::flagChest = 0;
+			}
+		}
+	}
+}
+
+//возможность изменять уровень из вне
+void Level::ChangeLevel(int x, int y, int LocationChange, int TextureChange) {
+	Location[x][y] = LocationChange;
+	textureLocation[x][y] = TextureChange;
+}
+
+//Обновление данных объектов
+void Level::handleEvents(SDL_Event eventInLvl)
+{
+	switch (eventInLvl.type)
+	{
+	case SDL_MOUSEBUTTONDOWN:
+
+		//Взаимодействие с Items в Inventory
+		uiInv->clickForItemsInInv();
+
+		//Взаимодействие с Equiped Items
+		uiEquiped->clickForItemsInInv();
+
+		//Вызов окна Spec по нажатию мыши
+		uiInfo->handleEvents(eventInLvl);
+
+		//Увеличение значения характеристик по нажатию мыши
+		uiSpec->handleEvents(eventInLvl);
+		
+		//Вызов окна Inventory по нажатию мыши
+		uiInv->handleEvents(eventInLvl);
+
+		if (eventInLvl.button.button == SDL_BUTTON_RIGHT)
+		{
+			//Вызов infoEnemy по нажатию мыши
+			uiEnemyHpInfo->callEnemyInfo();
+		}
+
+		
+	case SDL_KEYDOWN:
+		//Смена окон (с Spec на Info и наоборот) на Q
+		KeyboardButtonsInLevel::keyForCallSpecWin(keys);
+
+		//Вызов окна Inventory на I
+		KeyboardButtonsInLevel::keyForCallInvWin(keys);
+
+		//Увеличение характеристик Spec с помощью клавиш
+		KeyboardButtonsInLevel::keyForIncPlayerSpec(keys);
+
+	default:
+		break;
+	}
+
+	//Передача event в Player
+	if (player)
+	{
+		player->handleEvents(eventInLvl);
+	}
+}
+//rand для рандомного выбора метода генерации
+
+void Level::Generate() {
+	srand(time(0));
+	generateChoose = 5;
+	player->generate = generateChoose;
+	enemyTurtle->generate = generateChoose;
+	if (generateChoose == 0) {
+		ChunkGenerationMethod();
+	}
+	if (generateChoose == 4) {
+		LabGeneration();
+	}
+	if (generateChoose == 5) {
+		CastleLabGeneration();
+	}
+	if (generateChoose == 1) {
+		RoomGenerationMethod2();
+	}
+	if (generateChoose == 2) {
+		ChunkGenerationMethod2();
+	}
+	for (int i = 0; i < 3; i++) {
+		itemsOnLvl[i] = rand() % 4 + 1;
+	}
+	itemsHave = 2;
+	floorLvl++;
+}
---
+++ src/Managers.cpp
@@ -0,0 +1,182 @@
+﻿#pragma once
+#include "Managers.h"
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+#include <iostream>
+#include <string>
+#include <thread>
+
+SDL_Texture* textureManager::LoadTexture(const char* texName, SDL_Renderer* ren) 
+{
+	SDL_Texture* tex = IMG_LoadTexture(ren, texName);
+	return tex;
+}
+
+bool InputManager::MouseInArea(int x, int y, int w, int h, int MouseX, int MouseY) 
+{
+	if ((MouseX >= x) && (MouseX <= x + w) && (MouseY >= y) && (MouseY <= y + h)) 
+	{
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+}
+
+
+void RenderManager::CopyToRender(SDL_Texture* texture, SDL_Renderer* ren, int x, int y, int w, int h, int dx, int dy, int dw, int dh) 
+{
+	SDL_Rect COORD; SDL_Rect dCOORD;
+	COORD.x = x; COORD.y = y; COORD.w = w; COORD.h = h;
+	dCOORD.x = dx; dCOORD.y = dy; dCOORD.w = dw; dCOORD.h = dh;
+	SDL_RenderCopy(ren, texture, &dCOORD, &COORD);
+}
+void RenderManager::CopyToRender(SDL_Texture* texture, SDL_Renderer* ren, int x, int y, int w, int h) 
+{	
+	SDL_Rect COORD; SDL_Rect dCOORD;
+	COORD.x = x; COORD.y = y; COORD.w = w; COORD.h = h;
+	SDL_RenderCopy(ren, texture, NULL, &COORD);
+}	
+void RenderManager::CopyToRender(SDL_Texture* texture, SDL_Renderer* ren) 
+{
+	SDL_RenderCopy(ren, texture, NULL, NULL);
+}
+
+void RenderManager::SetTile(int x, int y, int tile, SDL_Renderer* renderer, SDL_Texture* TileTexture) {
+	switch (tile)
+	{
+	case 0:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 0, 0, 32, 32);
+		break;
+	case 1:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 0, 32, 32, 32);
+		break;
+	case 2:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 0, 64, 32, 32);
+		break;
+	case 3:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 0, 96, 32, 32);
+		break;
+	case 4:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 32, 0, 32, 32);
+		break;
+	case 5:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 32, 32, 32, 32);
+		break;
+	case 6:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 32, 64, 32, 32);
+		break;
+	case 7:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 32, 96, 32, 32);
+		break;
+	case 8:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 64, 0, 32, 32);
+		break;
+	case 9:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 64, 32, 32, 32);
+		break;
+	case 10:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 64, 64, 32, 32);
+		break;
+	case 11:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 64, 96, 32, 32);
+		break;
+	case 12:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 96, 0, 32, 32);
+		break;
+	case 13:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 96, 32, 32, 32);
+		break;
+	case 14:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 96, 64, 32, 32);
+		break;
+	case 15:
+		RenderManager::CopyToRender(TileTexture, renderer, x, y, 32, 32, 96, 96, 32, 32);
+		break;
+	default:
+		break;
+	}
+}
+
+SDL_Texture* FontManager::renderText(const char* text, const char* fontFile, SDL_Color textColor, int fontSize, SDL_Renderer* renderer) 
+{
+	SDL_Surface* surf;
+	SDL_Color fontColor;
+	TTF_Font* font;
+	SDL_Texture* fontTexture;
+	fontColor = textColor;
+	font = TTF_OpenFont(fontFile, fontSize);
+	if (font == NULL)
+	{
+		std::cout << "Error TTF_OpenFont"<< std::endl;
+		std::cout << TTF_GetError() << std::endl;
+		return nullptr;
+	}
+	surf = TTF_RenderText_Blended(font, text, fontColor);
+
+	fontTexture = SDL_CreateTextureFromSurface(renderer, surf);
+
+	SDL_FreeSurface(surf);
+	TTF_CloseFont(font);
+	return fontTexture;
+}
+
+Timer::Timer()
+{
+
+}
+
+void Timer::add(std::chrono::milliseconds delay, std::function<void()> callback, bool asynchronous)
+{
+	if (asynchronous)
+	{
+		std::thread([=]()
+			{
+			std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+			callback();
+			}).detach();
+	}
+	else 
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+		callback();
+	}
+}
+
+int FlagManager::flagPlayer = 1;
+int FlagManager::flagEnemy = 0;
+
+bool FlagManager::flagUI = 1;
+bool FlagManager::flagUiSpec = 0;
+bool FlagManager::flagUiEnemy = 0;
+bool FlagManager::flagInv = 0;
+
+bool FlagManager::flagCheckHP = 0;
+bool FlagManager::flagCheckMana = 0;
+bool FlagManager::flagCheckExp = 0;
+bool FlagManager::flagHaveDrop = 0;
+
+int FlagManager::flagChest = 0;
+int FlagManager::flagEquip = -1;
+int FlagManager::flagUnEquip = -1;
+int FlagManager::flagDrop = -1;
+
+bool FlagManager::flagSTR = 0;
+bool FlagManager::flagDEX = 0;
+bool FlagManager::flagINT = 0;
+bool FlagManager::flagWSD = 0;
+bool FlagManager::flagPHS = 0;
+bool FlagManager::flagLCK = 0;
+
+
+int FlagManager::flagMeleeAttackPlayer = 0;
+int FlagManager::flagMeleeAttackEnemy = 0;
+
+bool FlagManager::flagRangeAttack = 0;
+//check hp enemy
+bool FlagManager::flagCheckHpEnemy = 0;
+
+
+
